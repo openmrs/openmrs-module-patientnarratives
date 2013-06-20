6e24e1d96c475a2908e75f1fde3d43ec96320edd
harshadura <harshadura@gmail.com>
2013-06-20 10:32:22 +0530

    added a gutter to openmrs

---
+++ omod/src/main/java/org/openmrs/module/patientnarratives/extension/html/GutterListExt.java
@@ -0,0 +1,33 @@
+package org.openmrs.module.patientnarratives.extension.html;
+
+import org.openmrs.module.web.extension.LinkExt;
+
+public class GutterListExt extends LinkExt {
+
+    String urlg = "";
+    String label = "";
+
+    public String getLabel() {
+        label = "Patient Narratives";
+        return this.label;
+    }
+
+    public String getUrl() {
+        urlg = "http://www.google.com/";
+        return this.urlg;
+    }
+
+    /**
+     * Returns the required privilege in order to see this section. Can be a
+     * comma delimited list of privileges. If the default empty string is
+     * returned, only an authenticated user is required
+     *
+     * @return Privilege string
+     */
+    public String getRequiredPrivilege() {
+        return "";
+    }
+
+}
+
+
